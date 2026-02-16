3212bd2cbea6257fa70289a85ded26bc4fc88e7c
Malik Majeed <malikmajeedzia@gmail.com>
2026-02-16 16:01:43 +0500

    ui updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/compnenets/HeroSection";
 import ServicesSection from "@/compnenets/ServicesSection";
+import HowItWorkSection from "@/compnenets/HowItWorkSection";
 export default function Home() {
   return (
     <>
    <HeroSection />
 <ServicesSection />
+<HowItWorkSection />
 </>
   );
 }
