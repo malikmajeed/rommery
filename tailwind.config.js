a723a2c5cf8cef5ca5f62632699e720e7a3b931a
Malik Majeed <malikmajeedzia@gmail.com>
2026-02-16 14:46:38 +0500

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/compnenets/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ["var(--font-heading)"],
+        body: ["var(--font-body)"],
+      },
+    },
+  },
+  plugins: [],
+};
